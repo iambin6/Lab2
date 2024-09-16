ce2b449c165364a70fa4bbd986e9864823345418
iambin6 <miraisan2512@gmail.com>
2024-09-16 22:23:32 +0700

    Lession 2.2

--- Test.java
+++ Test.java
@@ -1,9 +1,35 @@
-package Lession1;
+package Lession2;
+
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         Person p = new Person(0, "", false);
-        p.scanInfo();
-        p.printInfo();
+        scanInfo(p);
+        printInfo(p);
+    }
+
+    public static void scanInfo(Person p) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter ID: ");
+        int id = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter Gender (true for male, false for female): ");
+        boolean gender = input.nextBoolean();
+
+        p.setId(id);
+        p.setName(name);
+        p.setMale(gender);
+    }
+
+    public static void printInfo(Person p) {
+        System.out.println("------------------------------");
+        System.out.println("| ID | Name | Male |");
+        System.out.printf("| %d | %s | %b |\n", p.getId(), p.getName(), p.isMale());
     }
 }
