903e7ec178b9b7f094de8076e12dce68f2de92a1
iambin6 <miraisan2512@gmail.com>
2024-09-16 22:21:43 +0700

    lession 1.2

---
+++ Test.java
@@ -0,0 +1,9 @@
+package Lession1;
+
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person(0, "", false);
+        p.scanInfo();
+        p.printInfo();
+    }
+}
